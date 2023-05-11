b3aff5de50ad329b1b1f501748123c91596168c9
Mac <mac@Macs-MacBook-Pro.local>
2023-05-12 00:40:58 +0545

    Created Related Middlewares

--- src/routes/books.ts
+++ src/routes/books.ts
@@ -1,5 +1,7 @@
 import express from "express";
 import { BooksController } from "../http/controllers/BooksController";
+import { AdminMiddleware } from "../http/middlewares/AdminMiddleware";
+import { AuthMiddleware } from "../http/middlewares/AuthMiddleware";
 import { ErrorHandler } from "../http/middlewares/ErrorHandler";
 import { FileUploader } from "../http/middlewares/FileUploader";
 
@@ -11,10 +13,22 @@ router.get("/", ErrorHandler.catchErrors(booksController.get));
 router.get("/:id", ErrorHandler.catchErrors(booksController.getBook));
 router.post(
   "/",
+  ErrorHandler.catchErrors(AuthMiddleware.authenticate),
+  ErrorHandler.catchErrors(AdminMiddleware.check),
   FileUploader.upload("image", "books", 2 * 1024 * 1024),
   ErrorHandler.catchErrors(booksController.create)
 );
-router.put("/:id", ErrorHandler.catchErrors(booksController.update));
-router.delete("/:id", ErrorHandler.catchErrors(booksController.delete));
+router.put(
+  "/:id",
+  ErrorHandler.catchErrors(AuthMiddleware.authenticate),
+  ErrorHandler.catchErrors(AdminMiddleware.check),
+  ErrorHandler.catchErrors(booksController.update)
+);
+router.delete(
+  "/:id",
+  ErrorHandler.catchErrors(AuthMiddleware.authenticate),
+  ErrorHandler.catchErrors(AdminMiddleware.check),
+  ErrorHandler.catchErrors(booksController.delete)
+);
 
 export default router;
